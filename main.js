44dba180b69a3b201d4d880adeb8ac52e2c81da9
TranVanSu2001 <tranvansu2001@gmail.com>
2023-06-03 02:09:33 +0700

    fix get id extension - add get name extension

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const axios = require("axios");
 const fs = require("fs");
 const JSZip = require("jszip");
+const puppeteer = require("puppeteer");
+
+// provide link to extension in Chrome Web Store
+const linkExtension =
+  "https://chrome.google.com/webstore/detail/compose-ai-ai-powered-wri/ddlbpiadoechcolndfeaonajmngmhblj?hl=en-US";
 
 // get id extension from link in Chrome Web Store
 function getExtensionId(link) {
-  const regex = /\/([a-z]{32})\?/i;
+  const regex = /\/([a-z]{32})/i;
   const matches = regex.exec(link);
   if (matches && matches.length > 1) {
     return matches[1];
@@ -12,18 +17,30 @@ function getExtensionId(link) {
   return null;
 }
 
-// provide link to extension in Chrome Web Store
-const linkExtension =
-  "https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en-US";
+// get name extension to name file
+const getExtensionName = async (link) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(link);
+    await page.waitForSelector("h1.e-f-w");
+    const nameElement = await page.$("h1.e-f-w");
+    const name = await page.evaluate(
+      (element) => element.textContent.trim(),
+      nameElement
+    );
+    await browser.close();
+    return name;
+  } catch (error) {
+    console.error("Error retrieving extension name:", error.message);
+    return "Unknown";
+  }
+};
 
 const extensionID = getExtensionId(linkExtension);
 
 // base link to get crx file
 const fileLink = `https://clients2.google.com/service/update2/crx?response=redirect&os=linux&arch=x64&os_arch=x86_64&nacl_arch=x86-64&prod=chromium&prodchannel=unknown&prodversion=91.0.4442.4&lang=en-US&acceptformat=crx2,crx3&x=id%3D${extensionID}%26installsource%3Dondemand%26uc`;
-
-// name file crx and zip
-const crxFilePath = "extension.crx";
-const zipFilePath = "extension.zip";
 
 // download crx file extension
 async function downloadFileCrx(linkUrl, filePath) {
@@ -79,8 +96,13 @@ function convertCRXtoZIP(crxFilePath, zipFilePath) {
 
 // conver crx to zip file
 const downloadZipExtension = async () => {
-  await downloadFileCrx(fileLink, crxFilePath);
+  const extensionName = await getExtensionName(linkExtension);
 
+  // name file crx and zip
+  const crxFilePath = `${extensionName}.crx`;
+  const zipFilePath = `${extensionName}.zip`;
+
+  await downloadFileCrx(fileLink, crxFilePath);
   await convertCRXtoZIP(crxFilePath, zipFilePath);
 
   console.log("----- success -----");
